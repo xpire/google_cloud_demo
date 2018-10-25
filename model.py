34506bd9e3b0de76844e54ca7c4cb16c9d8d293c
Justin Or <xpirep@gmail.com>
2018-10-26 10:04:41 +1100

    model created from linear regression

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 #credits: https://www.kaggle.com/elenapetrova/time-series-analysis-and-forecasts-with-prophet
 
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -65,52 +66,166 @@ def preprocess():
     # print(train_store.groupby('StoreType')['Customers','Sales','SalesPerCustomer'].sum())
     # onehot encoding of stores
     train_store['StoreTypeA'] = (train_store['StoreType'] == 'a')
-    train_store['StoreTypeB'] = (train_store['StoreType'] == 'b')
-    train_store['StoreTypeC'] = (train_store['StoreType'] == 'c')
-    train_store['StoreTypeD'] = (train_store['StoreType'] == 'd')
+    train_store['StoreTypeB'] =  (train_store['StoreType'] == 'b') 
+    train_store['StoreTypeC'] =  (train_store['StoreType'] == 'c') 
+    train_store['StoreTypeD'] =  (train_store['StoreType'] == 'd') 
     train_store = train_store.drop(columns = ['StoreType'])
 
+    # onehot encoding of stores
+    train_store['AssortA'] =  (train_store['Assortment'] == 'a') 
+    train_store['AssortB'] =  (train_store['Assortment'] == 'b') 
+    train_store['AssortC'] =  (train_store['Assortment'] == 'c') 
+    train_store = train_store.drop(columns = ['Assortment'])
+
     #onehot encoding of holidays
-    train_store['StateHolidayA'] = (train_store['StateHoliday'] == 'a')
-    train_store['StateHolidayB'] = (train_store['StateHoliday'] == 'b')
-    train_store['StateHolidayC'] = (train_store['StateHoliday'] == 'c')
+    train_store['StateHolidayA'] =  (train_store['StateHoliday'] == 'a') 
+    train_store['StateHolidayB'] =  (train_store['StateHoliday'] == 'b') 
+    train_store['StateHolidayC'] =  (train_store['StateHoliday'] == 'c') 
     train_store = train_store.drop(columns = ['StateHoliday'])
 
     #onehot encoding of promoInterval 
-    train_store['promoJan'] = ("Jan" in train_store['PromoInterval'])
-    train_store['promoFeb'] = ("Feb" in train_store['PromoInterval'])
-    train_store['promoMar'] = ("Mar" in train_store['PromoInterval'])
-    train_store['promoApr'] = ("Apr" in train_store['PromoInterval'])
-    train_store['promoMay'] = ("May" in train_store['PromoInterval'])
-    train_store['promoJun'] = ("Jun" in train_store['PromoInterval'])
-    train_store['promoJul'] = ("Jul" in train_store['PromoInterval'])
-    train_store['promoAug'] = ("Aug" in train_store['PromoInterval'])
-    train_store['promoSep'] = ("Sep" in train_store['PromoInterval'])
-    train_store['promoOct'] = ("Oct" in train_store['PromoInterval'])
-    train_store['promoNov'] = ("Nov" in train_store['PromoInterval'])
-    train_store['promoDec'] = ("Dec" in train_store['PromoInterval'])
+    train_store['promoJan'] = ("Jan" in train_store['PromoInterval']) 
+    train_store['promoFeb'] = ("Feb" in train_store['PromoInterval']) 
+    train_store['promoMar'] = ("Mar" in train_store['PromoInterval']) 
+    train_store['promoApr'] = ("Apr" in train_store['PromoInterval']) 
+    train_store['promoMay'] = ("May" in train_store['PromoInterval']) 
+    train_store['promoJun'] = ("Jun" in train_store['PromoInterval']) 
+    train_store['promoJul'] = ("Jul" in train_store['PromoInterval']) 
+    train_store['promoAug'] = ("Aug" in train_store['PromoInterval']) 
+    train_store['promoSep'] = ("Sep" in train_store['PromoInterval']) 
+    train_store['promoOct'] = ("Oct" in train_store['PromoInterval']) 
+    train_store['promoNov'] = ("Nov" in train_store['PromoInterval']) 
+    train_store['promoDec'] = ("Dec" in train_store['PromoInterval']) 
     train_store = train_store.drop(columns = ['PromoInterval'])
 
     #onehot encoding of DayOfWeek
-    train_store['DayMon'] = (1 == train_store["DayOfWeek"])
-    train_store['DayTue'] = (2 == train_store["DayOfWeek"])
-    train_store['DayWed'] = (3 == train_store["DayOfWeek"])
-    train_store['DayThu'] = (4 == train_store["DayOfWeek"])
-    train_store['DayFri'] = (5 == train_store["DayOfWeek"])
-    train_store['DaySat'] = (6 == train_store["DayOfWeek"])
-    train_store['DaySun'] = (7 == train_store["DayOfWeek"])
+    train_store['DayMon'] = (1 == train_store["DayOfWeek"]) 
+    train_store['DayTue'] = (2 == train_store["DayOfWeek"]) 
+    train_store['DayWed'] = (3 == train_store["DayOfWeek"]) 
+    train_store['DayThu'] = (4 == train_store["DayOfWeek"]) 
+    train_store['DayFri'] = (5 == train_store["DayOfWeek"]) 
+    train_store['DaySat'] = (6 == train_store["DayOfWeek"]) 
+    train_store['DaySun'] = (7 == train_store["DayOfWeek"]) 
     train_store = train_store.drop(columns = ['DayOfWeek'])
-
-    #produce the X and Y
-
-    train_store.to_csv(output_file, index = True)
+    return train_store
+    # print("Final: \n{}".format(train_store.head().to_string()))
+    # train_store.to_csv(output_file, index = True)
 
 #~~~~~~~~Loading~~~~~~~~
+def loading(train_store):
     print("#~~~~~~~~Loading~~~~~~~~")
-    #convert pandas dataframe to a numpy data frame
+    #separate the X and Y components
+    label = train_store['Sales'].values
+    train_store = train_store.drop(columns = ['Sales'])
+    #convert pandas dataframe to tensor
+    print(train_store.dtypes)
+    target = ['Sales']
+    features = ['Store', #'Sales', 
+    'Customers', 'Open', 'Promo', 'SchoolHoliday', 'Year', 'Month', 'Day', 'WeekofYear', #OG
+    'SalesPerCustomer', 'CompetitionDistance', 'CompetitionOpenSinceMonth', 'CompetitionOpenSinceYear', 'Promo2', 'Promo2SinceWeek', 'Promo2SinceYear', #floats except Promo2
+    'StoreTypeA', 'StoreTypeB', 'StoreTypeC', 'StoreTypeD', 
+    'AssortA', 'AssortB', 'AssortC', 
+    'StateHolidayA', 'StateHolidayB', 'StateHolidayC',
+    'promoJan', 'promoFeb', 'promoMar', 'promoApr', 'promoMay', 'promoJun', 'promoJul', 'promoAug', 'promoSep', 'promoOct', 'promoNov', 'promoDec',
+    'DayMon', 'DayTue', 'DayWed', 'DayThu', 'DayFri', 'DaySat', 'DaySun'] #Booleans
+    features_dict = {features[i] : i for i in range(len(features))}
+    print(features_dict)
+    train_store = train_store.values
+
+    print(train_store)
+
+    #take 80% for training, 20% for validation
+    total = len(train_store)
+    idx = np.random.permutation(total)
+    x,y = train_store[idx], label[idx]
+    print("x:\n{}".format(x))
+    print("y:\n{}".format(y))
+
+    train_x = x[:int(total*0.8)]
+    train_y = y[:int(total*0.8)]
+    test_x = x[int(total*0.8):]
+    test_y = y[int(total*0.8):]
+
+    print("Train_x:\n{}".format(train_x))
+    print("Train_y:\n{}".format(train_y))
+    print("Test_x:\n{}".format(test_x))
+    print("Test_y:\n{}".format(test_y))
+
+    print("Lengths: train ({}, {}), test ({}, {})".format(len(train_x), len(train_y), len(test_x), len(test_y)))
+    avg = np.mean(train_y)
+    rmse = np.sqrt(np.mean((test_y - avg)**2))
+    print("Baseline to beat w/ avg -> {}: {}\n".format(avg, rmse))
+    return train_x, train_y, test_x, test_y
 
 #~~~~~~~~Training~~~~~~~~
+def train(train_x, train_y, test_x, test_y):
     print("#~~~~~~~~Training~~~~~~~~")
+    #~~~~~~~~VARIABLES~~~~~~~~
+    NPREDICTORS = len(features)
+    NOUTPUTS = len(target)
+    NHIDDEN = 20
+    NUMITERATIONS = 10000
+
+    with tf.Session() as sess:
+        feature_data = tf.placeholder("float32", [None, NPREDICTORS])
+        target_data = tf.placeholder("float32", [None, NOUTPUTS])
+        w1 = tf.Variable(tf.truncated_normal([NPREDICTORS, NOUTPUTS], stddev= 0.0))
+        b1 = tf.Variable(tf.ones([NOUTPUTS]))
+        logits = tf.matmul(feature_data, w1) + b1
+        # preds = tf.nn.relu(logits)
+        preds = logits #linear regression requires no activation
+        cost = tf.losses.mean_squared_error(
+            labels = target_data,
+            predictions = preds           
+        )
+        accuracy = cost/feature_data.shape
+        training_step = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+        init = tf.initialize_all_variables()
+        sess.run(init)
+
+        saver = tf.train.Saver({'weights' : w1, 'bias' : b1})
+
+        for iter in range(0, NUMITERATIONS):
+            sess.run(
+                training_step,
+                feed_dict= {
+                    feature_data : train_x,
+                    target_data : train_y.reshape(len(train_x), NOUTPUTS)
+                }
+            )
+            if (iter % 10 == 0):
+                cost_value = sess.run(
+                    [cost],
+                    feed_dict= {
+                        feature_data : train_x,
+                        target_data : train_y.reshape(len(train_x), NOUTPUTS)
+                    })
+                print("Iteration: ", iter)
+                print("loss", cost_value)
+            
+            if iter % 50 == 0:
+                test(test_x, test_y, n, saver, sess)
+
+        # filename = saver.save(sess, "./checkpoints", global_step=NUMITERATIONS)
+        # print("Model written to {}".format(filename))
+        # print("Test error: {}".format(
+        #     np.sqrt(cost.eval(feed_dict= {
+        #         feature_data : test_x,
+        #         target_data : test_y.reshape(len(test_x), NOUTPUTS)
+        #     })) /len(test_x)
+        # ))
+
+def test(test_x, test_y, n, saver, sess):
+    filename = saver.save(sess, "./checkpoints", global_step=n)
+    print("Model written to {}".format(filename))
+    print("Test error: {}".format(
+        np.sqrt(cost.eval(feed_dict= {
+            feature_data : test_x,
+            target_data : test_y.reshape(len(test_x), NOUTPUTS)
+        })) /len(test_x)
+    ))
+
+
 
 # if __name__ == "__main__":
     # import argparse
@@ -118,6 +233,8 @@ def preprocess():
     # parser = argparse.ArgumentParser()
     # parser.add_argument("mode", choices=["preprocess", "train", "eval"])
     
+    # args = parser.parse_args()
+
     # if (args.mode == "train"):
     #     preprocess()
     #     print("Training Run")
@@ -129,4 +246,5 @@ def preprocess():
     #     print("Preprocessing")
     #     preprocess()
 
-preprocess()
+train_x, train_y, test_x, test_y = loading(preprocess())
+train(train_x, train_y, test_x, test_y)
